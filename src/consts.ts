c588f4b677b8f37f7cb66b273008344ab973e0c1
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2024-01-14 14:29:55 -0800

    chore: READMEを更新

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "ITトリオの日常";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "ITトリオの日常の公式サイトです。";
