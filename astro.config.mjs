17f2a22438c79edd718907c61c19f971665c37c2
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2024-01-14 15:33:44 -0800

    chore: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://it-trio.github.io',
-  base:'/it-trio-web',
+  site: "https://it-trio.github.io",
+  base: "/it-trio-web",
   integrations: [mdx(), sitemap()],
 });
