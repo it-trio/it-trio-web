896680839a44450727466508b10fbd816cec5b40
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2024-01-13 20:59:18 -0800

    chore: pretterを導入

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,33 +1,31 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
-
 
 const episode = defineCollection({
-	type: 'data',
-	schema: z.object({
-		guid: z.string(),
-		title: z.string(),
-		description: z.string(),
-		number: z.number(),
-		season: z.number(),
-		pubDate: z.coerce.date(),
-		image: z.string(),
-		duration: z.string(),
-		url: z.string(),
-	}),
+  type: "data",
+  schema: z.object({
+    guid: z.string(),
+    title: z.string(),
+    description: z.string(),
+    number: z.number(),
+    season: z.number(),
+    pubDate: z.coerce.date(),
+    image: z.string(),
+    duration: z.string(),
+    url: z.string(),
+  }),
 });
-
 
 export const collections = { blog, episode };
