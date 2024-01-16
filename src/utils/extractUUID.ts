88b68381e549444fbcef9d5aa24ee5ee59fa1f38
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2024-01-15 23:15:03 -0800

    feat: トップに表示するエピソードの修正

--- src/utils/extractUUID.ts
+++ src/utils/extractUUID.ts
@@ -1,8 +1,8 @@
 export default function extractUUID(url: string): string | null {
- // Regular expression for UUID v4
- const uuidRegex =
-   /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
- const match = url.match(uuidRegex);
+  // Regular expression for UUID v4
+  const uuidRegex =
+    /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
+  const match = url.match(uuidRegex);
 
- return match ? match[0] : null;
+  return match ? match[0] : null;
 }
