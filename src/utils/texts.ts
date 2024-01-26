bad4a09a3f81d0d4b6bf9562b96736cade7cb3f3
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2024-01-25 16:20:03 -0800

    chore: format

--- src/utils/texts.ts
+++ src/utils/texts.ts
@@ -1,5 +1,4 @@
-
 // 先頭の改行を削除し、連続する改行を1つにする
-export const removeUnnecessaryNewLines = (text: string):string => {
+export const removeUnnecessaryNewLines = (text: string): string => {
   return text.replace(/^\n+/, "").replace(/\n+/g, "\n");
-}
+};
