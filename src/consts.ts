c4907ebb9803cf449d24c1351bbea1cababf2354
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2024-02-25 23:32:21 -0800

    feat: 色々

--- src/consts.ts
+++ src/consts.ts
@@ -2,19 +2,13 @@ export const SITE_TITLE = "ITトリオの日常";
 export const SITE_DESCRIPTION = "ITトリオの日常の公式サイトです。";
 
 export const RSS_FEED_URL = "https://rss.art19.com/it-pitpa";
-export const SPOTIFY_SHOW_LINK =
-  "https://open.spotify.com/show/4swQbE6pLzOz3p1Z9Etkqc";
-export const APPLE_PODCAST_SHOW_LINK =
-  "https://podcasts.apple.com/jp/podcast/it%E3%83%88%E3%83%AA%E3%82%AA%E3%81%AE%E6%97%A5%E5%B8%B8-%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A23%E4%BA%BA%E3%81%8C%E3%82%AB%E3%82%B8%E3%83%A5%E3%82%A2%E3%83%AB%E3%81%AB%E5%AD%A6%E3%81%B3%E3%82%92%E6%B7%B1%E3%82%81%E3%82%8B%E3%83%A9%E3%82%B8%E3%82%AA/id1644482809";
-export const AMAZON_MUSIC_SHOW_LINK =
-  "https://music.amazon.co.jp/podcasts/fdfe7e3f-4ddb-4717-9501-414e5dabcf3b/itトリオの日常-エンジニア3人がカジュアルに学びを深めるラジオ";
-export const YOUTUBE_MUSIC_SHOW_LINK =
-  "https://music.youtube.com/playlist?list=PLqtv1UJuRQssCX_himpcWZLalPSeHIcR3&si=7FplzKGZJZp2Gci2";
+export const SPOTIFY_SHOW_LINK = "https://open.spotify.com/show/4swQbE6pLzOz3p1Z9Etkqc";
+export const APPLE_PODCAST_SHOW_LINK = "https://podcasts.apple.com/jp/podcast/it%E3%83%88%E3%83%AA%E3%82%AA%E3%81%AE%E6%97%A5%E5%B8%B8-%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A23%E4%BA%BA%E3%81%8C%E3%82%AB%E3%82%B8%E3%83%A5%E3%82%A2%E3%83%AB%E3%81%AB%E5%AD%A6%E3%81%B3%E3%82%92%E6%B7%B1%E3%82%81%E3%82%8B%E3%83%A9%E3%82%B8%E3%82%AA/id1644482809";
+export const AMAZON_MUSIC_SHOW_LINK = "https://music.amazon.co.jp/podcasts/fdfe7e3f-4ddb-4717-9501-414e5dabcf3b/itトリオの日常-エンジニア3人がカジュアルに学びを深めるラジオ";
+export const YOUTUBE_MUSIC_SHOW_LINK = "https://music.youtube.com/playlist?list=PLqtv1UJuRQssCX_himpcWZLalPSeHIcR3&si=7FplzKGZJZp2Gci2";
 
 export const GOOGLE_FORM_LINK = "https://forms.gle/sqzWk2Yb79cMvFGg8";
 
-export const POPULAR_EPISODE_GUID =
-  "zM74U6lSzfptPHb7JBjCXQD1iXKMHHGz3k53s6C_hWc";
-export const POPULAR_EPISODE_GUID_2 =
-  "6aFYEgyMEejSoEd1XvMZS3UAjPcQxSkNdfzPqSoIuQU";
+export const POPULAR_EPISODE_GUID = "zM74U6lSzfptPHb7JBjCXQD1iXKMHHGz3k53s6C_hWc";
+export const POPULAR_EPISODE_GUID_2 = "6aFYEgyMEejSoEd1XvMZS3UAjPcQxSkNdfzPqSoIuQU";
 export const POPULAR_EPISODE_GUID_3 = "a7113709-892a-4cd5-9be7-c188292691f5";
