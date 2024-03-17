e6bb8e76e45f5dd1b31d804575ba2e7481a1b330
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2024-03-16 19:24:39 -0700

    feat: sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://it-trio-no.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap()]
 });
