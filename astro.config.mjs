1c5b0d52ef4177c7994f84474c11a7c91909bbc3
Yugo Ogura <15419227+Spice-Z@users.noreply.github.com>
2024-03-18 00:34:18 -0700

    Update content (#56)
    Co-authored-by: GitHub <noreply@github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://it-trio-no.com",
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap()],
 });
