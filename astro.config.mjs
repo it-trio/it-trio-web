8fc06574a40181a659a8121b7b4fa0eb6edd9bee
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2024-06-11 16:13:43 -0700

    fix: config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://it-trio-no.com",
+  trailingSlash: "always",
   integrations: [mdx(), sitemap()],
 });
